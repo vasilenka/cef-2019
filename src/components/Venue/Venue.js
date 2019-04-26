6c212941dcb32ab398b63cec3c2bf568b3810cba
Ongki Herlambang <meepomodeon@gmail.com>
2019-04-26 07:53:57 +0700

    hide venue link

--- src/components/Venue/Venue.js
+++ src/components/Venue/Venue.js
@@ -24,7 +24,7 @@ const Venue = ({
           Solo, Central Java, Indonesia
         </Text>
         <Box>
-          <Link to={'/venue'}><Button primary small style={{marginRight: '12px'}}>How to get there?</Button></Link>
+          {/* <Link to={'/venue'}><Button primary small style={{marginRight: '12px'}}>How to get there?</Button></Link> */}
           <a
             href="https://www.google.com/maps/place/Novotel+Solo/@-7.5679198,110.8149673,17z/data=!3m1!4b1!4m5!3m4!1s0x2e7a1687812c1277:0x69f00f2d4cfc3485!8m2!3d-7.5679198!4d110.817156"
             target="_blank"
