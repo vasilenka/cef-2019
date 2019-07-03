98fa07c0b4dbde71a7bc85469844fe5734b8fdcb
Ongki Herlambang <meepomodeon@gmail.com>
2019-07-03 12:29:26 +0700

    combine logo

---
+++ src/components/images/organizers/combine.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+const Image = ({url, ...restProps}) => {
+  return(
+    <StaticQuery
+      query={graphql`
+        query {
+          placeholderImage: file(relativePath: { eq: "organizer/combine.jpg" }) {
+            childImageSharp {
+              fluid(maxWidth: 300) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      `}
+      render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+    />
+  )
+}
+export default Image
