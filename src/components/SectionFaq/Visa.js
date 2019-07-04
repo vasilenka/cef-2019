d30b3cff081b542a888ef4b4a769660384cdc789
Ongki Herlambang <meepomodeon@gmail.com>
2019-07-04 10:09:16 +0700

    update request form link

--- src/components/SectionFaq/Visa.js
+++ src/components/SectionFaq/Visa.js
@@ -14,7 +14,7 @@ const Visa = ({
         Nationals from specific countries and territories may enter Indonesia without visa or can apply for visa on arrival at the airport. Please visit <strong><a href="https://www.indonesia.travel/gb/en/trip-ideas/visa-free-for-169-countries-to-travel-to-indonesia" rel="noopener noreferrer" target="_blank">the Indonesian Immigration website</a></strong> for further details.
       </Text>
       <Text medium component="p" style={{marginBottom: '0'}}>
-        If you need a visa, please fill up the form (<strong><a href="https://www.indonesia.travel/gb/en/trip-ideas/visa-free-for-169-countries-to-travel-to-indonesia" rel="noopener noreferrer" target="_blank">get the form here</a></strong>) and send it to the Secretariat at <strong><a href="mailto:info@civicengagementforum.net">info@civicengagementforum.net</a></strong> together with your passport copy (photo page).
+        If you need a visa, please fill up the form (<strong><a href="https://drive.google.com/uc?authuser=0&id=1kli5kPVE6qUzPY4b53aqZH6hJJNsifBA&export=download" rel="noopener noreferrer" target="_blank">get the form here</a></strong>) and send it to the Secretariat at <strong><a href="mailto:info@civicengagementforum.net">info@civicengagementforum.net</a></strong> together with your passport copy (photo page).
         <br/>We will come back to you as soon as possible. In case you are not an invited speaker, the Organizing Committee is obliged to verify your background information prior to agreeing to issue the visa endorsement letter.
       </Text>
     </section>
