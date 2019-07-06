312f991e7d28599f6358779f8c847f8d12c08c74
Ongki Herlambang <meepomodeon@gmail.com>
2019-07-06 15:09:59 +0700

    refine config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteMetadata: {
     title: `Civic Engagement 4.0, 2019 International Forum in Solo`,
     description: `Civic Engagement 4.0: Dignity ~ Justice ~ Sustainability is a regional platform for mutual learning, action and advocacy, working toward achieving dignity, justice and sustainability in Southeast Asian (SEA) society, where the wave of Fourth Industrial Revolution is affecting each of us living in the region, we become increasingly aware of the need for multi-stakeholder collaboration which can effectively respond to it. The platform is expected to facilitate exchanges of knowledge for further collaboration in implementing actions and advocacy to achieve dignity, justice and sustainability among active members of communities working on the ground at the national, regional and international levels.`,
-    author: `kotakita.org - meridian.id - ongki 'vasilenka' herlambang <ongki@herlambang.id>`,
+    author: `Kotakita.org - Meridian.id - ongki 'vasilenka' herlambang <ongki@herlambang.id>`,
     siteUrl: `https://civicengagementforum.net/solo/`
   },
   plugins: [
