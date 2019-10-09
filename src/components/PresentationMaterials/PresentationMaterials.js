e4ca44036d52e9411dbd6dfd8195baa8a81f20d1
Ongki Herlambang <meepomodeon@gmail.com>
2019-10-10 00:02:00 +0700

    presentation materials

--- src/components/PresentationMaterials/PresentationMaterials.js
+++ src/components/PresentationMaterials/PresentationMaterials.js
@@ -1,16 +1,89 @@
 import styles from "./PresentationMaterials.module.scss"
 import React from "react"
 import cx from "classnames"
+
 import Container from "../../layouts/Container/Container"
 import Text from "../../primitives/Text/Text"
+import { MayorsSymposium, PublicForum } from "./materials"
+import Download from "./../icons/download.inline.svg"
+
+const Item = ({ item }) => {
+  return (
+    <li className={styles.item}>
+      <Text heading5 component="p" style={{ marginBottom: 0, paddingRight: 24 }}>
+        {item.title}
+      </Text>
+      {item.url.length > 1 ? (
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          {item.url.map((link, index) => (
+            <a
+              key={`itemurl-${index}`}
+              className={cx(styles.download, styles.downloadAlt)}
+              href={link}
+              target="_blank"
+              rel="noreferrer noopener">
+              <Download style={{ display: "block", marginRight: 6 }} />{" "}
+              <Text heading6>{index === 0 ? "ENG" : "BHS"}</Text>
+            </a>
+          ))}
+        </div>
+      ) : (
+        <a
+          className={styles.download}
+          href={item.url[0]}
+          target="_blank"
+          rel="noreferrer noopener">
+          <Download style={{ display: "block" }} />
+        </a>
+      )}
+    </li>
+  )
+}
 
 const PresentationMaterials = ({ children, className, ...restProps }) => {
   return (
     <section className={cx(styles.root)} {...restProps}>
       <Container narrow>
-        <Text large component="p">
-          Das good text!
-        </Text>
+        <main style={{ marginBottom: 96 }}>
+          <Text heading2 component="h2" style={{ marginBottom: 48 }}>
+            Mayors Symposium
+          </Text>
+          {MayorsSymposium.map(
+            (item, itemIndex) =>
+              item.docs.length > 0 && (
+                <section className={styles.segment} key={`item-${itemIndex}`}>
+                  <Text heading3 component="p" className={styles.segmentName}>
+                    {item.name}
+                  </Text>
+                  <ul className={styles.segmentFiles}>
+                    {item.docs.map((doc, docIndex) => (
+                      <Item key={`doc-${docIndex}`} item={doc} />
+                    ))}
+                  </ul>
+                </section>
+              )
+          )}
+        </main>
+        <main>
+          <Text heading2 component="h2" style={{ marginBottom: 48 }}>
+            Public Forums
+          </Text>
+          {PublicForum.map(
+            (item, itemIndex) =>
+              item.docs.length > 0 && (
+                <section className={styles.segment} key={`item-${itemIndex}`}>
+                  <Text heading3 component="p" className={styles.segmentName}>
+                    {item.name}
+                  </Text>
+                  <ul className={styles.segmentFiles}>
+                    {item.docs.map((doc, docIndex) => (
+                      <Item key={`doc-${docIndex}`} item={doc} />
+                    ))}
+                  </ul>
+                </section>
+              )
+          )}
+        </main>
       </Container>
     </section>
   )
---
+++ src/components/PresentationMaterials/materials.js
@@ -0,0 +1,208 @@
+export const MayorsSymposium = [
+  {
+    name: "Interface I",
+    docs: [
+      {
+        title: "The Connected dots of inclusive education",
+        url: [
+          "https://drive.google.com/uc?id=1uOnNkPTvcmds_CZGFFrBH22YrE8Y4tDp&export=download",
+        ],
+      },
+      {
+        title: "Civil Society - Inclusive and Sustainable Cities",
+        url: [
+          "https://drive.google.com/uc?id=1iN_GlkHW1C4spJ2NFL6Y9YJDnchA8VNy&export=download",
+        ],
+      },
+      {
+        title:
+          "The Road to Inclusive and Sustainable Cities: Using Human Rights Based Approach",
+        url: [
+          "https://drive.google.com/uc?id=1yY_-L4Z5PWSUFRL3Fbi2uQioclxlog8I&export=download",
+        ],
+      },
+    ],
+  },
+  {
+    name: "Interface II",
+    docs: [
+      {
+        title: "The Religious Resonance of Urban Soundscape",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1oOos7fiR7xYm54kaKcoeDoQpWmHs3kHe&export=download",
+        ],
+      },
+      {
+        title: "Faith and Humanity in The City",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1N4oWa14AkY66ithdkl77nh3cl_Pm1B2i&export=download",
+        ],
+      },
+      {
+        title: "Faith and Human Dignity in the City",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1zLc2uDpuXAg3R8dvRXSh2xInkqEmVGjh&export=download",
+        ],
+      },
+    ],
+  },
+]
+
+export const PublicForum = [
+  {
+    name: "Keynote",
+    docs: [
+      {
+        title:
+          "The Good City? From the City as Cradle of Civilisation to the Sustainable Cities of Tomorrow",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=14URUaGuRKHuMolGcf5YtKiGz7DGv01kV&export=download",
+        ],
+      },
+      {
+        title: "Kampung Commoning: Innovation and agency",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1EaQeSB4bOQRvy-1IZ7mIZF60_0_g_cEV&export=download",
+        ],
+      },
+      {
+        title:
+          "The Role of Technology in Empowering Persons with Disabilities in The Formal and Informal Sectors",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1LT-Ttjyopoo9vWOC_3nUjeoF0VzeiLE8&export=download",
+        ],
+      },
+      {
+        title: "Thisable Enterprise Credential",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1cpVxR5g9M5D68k5z12RWcjD7JgmphgL7&export=download",
+        ],
+      },
+    ],
+  },
+  {
+    name: "Mobility in the Changing Landscape of Southeast Asia",
+    docs: [
+      {
+        title: "Circular Migration in Surakarta",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=139utPo_TVlG_rX-MD7fuV9fiQNuH_Jyh&export=download",
+        ],
+      },
+    ],
+  },
+  {
+    name: "Showcasing Innovative Products for Urban Living",
+    docs: [
+      {
+        title: "DTRON Smart Chair",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1wh1YKBPQKDaUyfD6vBQjId3RdVCxdQ7a&export=download",
+        ],
+      },
+    ],
+  },
+  {
+    name: "Education 4.0",
+    docs: [
+      {
+        title:
+          "Education 4.0 Knowledge – Innovation – Civic Engagement towards Global Leadership",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1j0RVgH-19oJGS3rDHUtWXuro5LMVyQ2Q&export=download",
+        ],
+      },
+      {
+        title: "Education 4.0",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1vbVK9alg0Ir6BiIt-QgAFF-xQ8Q3daXY&export=download",
+        ],
+      },
+      {
+        title: "Education 4.0 and University Social Responsibility (USR)",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1SrvfeQWu0uQO_mntbyghMCS-42Binxje&export=download",
+        ],
+      },
+    ],
+  },
+  {
+    name: "Beyond Waste Management",
+    docs: [
+      {
+        title: "The Co-Design Approach in Creating Inclusive Public Space for Children",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1hJ9XFCUiM31ZmKE7VPQCAcHbS1RY61mL&export=download",
+        ],
+      },
+      {
+        title: "Plastic - A Global and Local Problem",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1CZ2vnVSuNDgrrphOzk_eXmuCS8volYdf&export=download",
+        ],
+      },
+      {
+        title: "We are proud Bali is Clean 2025",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1qi48IWbsl0cpRZHHjOtpiLDnPvb_qj0C&export=download",
+        ],
+      },
+      {
+        title: "Sustainable Development Goals (SDGs) & Organic Cycle",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1nswElNF-8jrDplROIKcwlmC0R5aCXXhP&export=download",
+        ],
+      },
+    ],
+  },
+  {
+    name: "Memory as Resilience",
+    docs: [],
+  },
+  {
+    name: "Civic Engagement, a Strategic Lens",
+    docs: [
+      {
+        title: "Public Engagement: A Strategic Lens",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1qn8dz2C5w9-rWeW6btYeUDg3GiSlaw_X&export=download",
+          "https://drive.google.com/uc?authuser=0&id=1mKO63aVnE12jbAT1vb5XwFLDRbZes1Aa&export=download",
+        ],
+      },
+      {
+        title: "Principles for Collaborative Public Engagement",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1j_YsghBhgV6QBYMa7_2IDO0BOXrG-ske&export=download",
+        ],
+      },
+      {
+        title: "Surakarta Menuju Kota Inklusif",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=16ptB99SxsSthj3jCWXDRON-uSGTGy1hd&export=download",
+        ],
+      },
+      {
+        title: "Surakarta City Engagement",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1jzhHKUSPDQhiWOEmcgOCofKdksaKlaAj&export=download",
+        ],
+      },
+    ],
+  },
+  {
+    name: "Urban Farming",
+    docs: [],
+  },
+  {
+    name: "Listening Partnerships",
+    docs: [
+      {
+        title:
+          "Listening Partnerships: Living Fully and Leading with Integrity in a Time of Crisis",
+        url: [
+          "https://drive.google.com/uc?authuser=0&id=1TZy6OnQtmMjU-CBUpTHqrd4VnDI0QxiO&export=download",
+        ],
+      },
+    ],
+  },
+]
