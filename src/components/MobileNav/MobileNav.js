49ddbf377b4101e7f1097ae686488f4409153f40
Ongki Herlambang <meepomodeon@gmail.com>
2019-10-11 00:12:00 +0700

    add mobile menu

--- src/components/MobileNav/MobileNav.js
+++ src/components/MobileNav/MobileNav.js
@@ -1,107 +1,111 @@
-import styles from './MobileNav.module.scss'
-import React, { useState, useLayoutEffect } from 'react'
-import cx from 'classnames'
-import { Link } from 'gatsby'
+import styles from "./MobileNav.module.scss"
+import React, { useState, useLayoutEffect } from "react"
+import cx from "classnames"
+import { Link } from "gatsby"
 
-import Logo from './../../assets/images/logo.png'
+import Logo from "./../../assets/images/logo.png"
 
-const Trigger = ({className, ...restProps}) => {
+const Trigger = ({ className, ...restProps }) => {
   return (
-    <div className={styles.trigger} {...restProps}>menu</div>
+    <div className={styles.trigger} {...restProps}>
+      menu
+    </div>
   )
 }
 
 const Dialog = props => {
-
   useLayoutEffect(() => {
-    document.body.style.overflow = 'hidden'
+    document.body.style.overflow = "hidden"
     return () => {
-      document.body.style.overflow = 'auto'
+      document.body.style.overflow = "auto"
     }
   }, [])
 
   return (
     <div className={styles.dialogContainer}>
       <nav className={cx(styles.dialog)}>
-        <img className={styles.brand} src={Logo} alt="Civic Engagement 4.0"/>
+        <img className={styles.brand} src={Logo} alt="Civic Engagement 4.0" />
         <ul className={styles.wrapper}>
           <Link to="/" activeClassName={styles.active}>
-            <li className={styles.itemWrapper}>
-              Home
-            </li>
+            <li className={styles.itemWrapper}>Home</li>
           </Link>
           <Link to="/about" activeClassName={styles.active}>
-            <li className={styles.itemWrapper}>
-              About
-            </li>
+            <li className={styles.itemWrapper}>About</li>
           </Link>
-          <Link to="/venue"  activeClassName={styles.active}>
-            <li className={styles.itemWrapper}>
-              Venue
-            </li>
+          <Link to="/venue" activeClassName={styles.active}>
+            <li className={styles.itemWrapper}>Venue</li>
           </Link>
-          <Link to="/venue#about-the-sunan-hotel" onClick={() => props.setVisible(false)} activeClassName={styles.active}>
-            <li className={cx(styles.itemWrapper, styles.secondary)}>
-              About The Sunan Hotel
-            </li>
+          <Link
+            to="/venue#about-the-sunan-hotel"
+            onClick={() => props.setVisible(false)}
+            activeClassName={styles.active}>
+            <li className={cx(styles.itemWrapper, styles.secondary)}>About The Sunan Hotel</li>
           </Link>
-          <Link to="/venue#solo-at-a-glance" onClick={() => props.setVisible(false)} activeClassName={styles.active}>
-            <li className={cx(styles.itemWrapper, styles.secondary)}>
-              Solo at A Glance
-            </li>
+          <Link
+            to="/venue#solo-at-a-glance"
+            onClick={() => props.setVisible(false)}
+            activeClassName={styles.active}>
+            <li className={cx(styles.itemWrapper, styles.secondary)}>Solo at A Glance</li>
           </Link>
-          <Link to="/venue#pratical-information" onClick={() => props.setVisible(false)} activeClassName={styles.active}>
-            <li className={cx(styles.itemWrapper, styles.secondary)}>
-              Practical Information
-            </li>
+          <Link
+            to="/venue#pratical-information"
+            onClick={() => props.setVisible(false)}
+            activeClassName={styles.active}>
+            <li className={cx(styles.itemWrapper, styles.secondary)}>Practical Information</li>
           </Link>
-          <Link to="/organizers" onClick={() => props.setVisible(false)} activeClassName={styles.active}>
-            <li className={styles.itemWrapper}>
-              Organizers
-            </li>
+          <Link
+            to="/organizers"
+            onClick={() => props.setVisible(false)}
+            activeClassName={styles.active}>
+            <li className={styles.itemWrapper}>Organizers</li>
           </Link>
-          <Link to="/organizers#funding-partners" onClick={() => props.setVisible(false)} activeClassName={styles.active}>
-            <li className={cx(styles.itemWrapper, styles.secondary)}>
-              Funding Partners
-            </li>
+          <Link
+            to="/organizers#funding-partners"
+            onClick={() => props.setVisible(false)}
+            activeClassName={styles.active}>
+            <li className={cx(styles.itemWrapper, styles.secondary)}>Funding Partners</li>
           </Link>
           <Link to="/programs" activeClassName={styles.active}>
-            <li className={styles.itemWrapper}>
-              Programs
-            </li>
+            <li className={styles.itemWrapper}>Programs</li>
           </Link>
           <Link to="/speakers" activeClassName={styles.active}>
-            <li className={styles.itemWrapper}>
-              Speakers
-            </li>
+            <li className={styles.itemWrapper}>Speakers</li>
           </Link>
-          <Link to="/faq"  activeClassName={styles.active}>
-            <li className={styles.itemWrapper}>
-              FAQ
-            </li>
+          <Link to="/faq" activeClassName={styles.active}>
+            <li className={styles.itemWrapper}>FAQ</li>
+          </Link>
+          <li className={styles.itemWrapper}>Archives</li>
+          <Link
+            to="/presentation-materials"
+            onClick={() => props.setVisible(false)}
+            activeClassName={styles.active}>
+            <li className={cx(styles.itemWrapper, styles.secondary)}>Presentation Materials</li>
+          </Link>
+          <Link
+            to="/media"
+            onClick={() => props.setVisible(false)}
+            activeClassName={styles.active}>
+            <li className={cx(styles.itemWrapper, styles.secondary)}>Event Video and Photos</li>
           </Link>
         </ul>
-        <button type="button" className={styles.close} onClick={() => props.setVisible(!props.visible)}>Close</button>
+        <button
+          type="button"
+          className={styles.close}
+          onClick={() => props.setVisible(!props.visible)}>
+          Close
+        </button>
       </nav>
     </div>
   )
 }
 
-const MobileNav = ({
-  className,
-  ...restProps
-  }) => {
-
+const MobileNav = ({ className, ...restProps }) => {
   const [visible, setVisible] = useState(false)
 
   return (
     <>
-      {!visible &&
-        <Trigger onClick={() => setVisible(!visible)}/>
-      }
-      {
-        visible && <Dialog visible={visible} setVisible={setVisible} />
-      }
+      {!visible && <Trigger onClick={() => setVisible(!visible)} />}
+      {visible && <Dialog visible={visible} setVisible={setVisible} />}
     </>
   )
 }
